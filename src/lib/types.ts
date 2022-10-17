88b4693224e33a5cb7c53997b0bc4c7090233678
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-17 20:20:45 +0530

    feat: implement sanity helper functions

---
+++ src/lib/types.ts
@@ -0,0 +1,23 @@
+import { MDXRemoteSerializeResult } from 'next-mdx-remote';
+
+export type Post = {
+  _id: string;
+  slug: string;
+  content: MDXRemoteSerializeResult;
+  title: string;
+  date: string;
+  excerpt: string;
+  coverImage: string;
+  readingTime: string;
+  tweets: any[];
+};
+
+export type NowPlayingSong = {
+  album: string;
+  albumImageUrl: string;
+  artist: string;
+  isPlaying: boolean;
+  songUrl: string;
+  title: string;
+};
+
