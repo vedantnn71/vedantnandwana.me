7d1dc073eb4122e515515fd2e562d418f5283f6a
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-18 15:47:11 +0530

    fix(icons): invalid svg jsx properties

--- src/components/icons/menuIcon.tsx
+++ src/components/icons/menuIcon.tsx
@@ -7,9 +7,9 @@ export function MenuIcon(props: JSX.IntrinsicElements["svg"]) {
       height={32}
       fill="none"
       stroke={"#fff"}
-      stroke-width="1"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
       <line x1="21" x2="3" y1="18" y2="18"></line>
