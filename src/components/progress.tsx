26cb7e45e4f7c6954d0e2edb38de50cbdd500167
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-18 18:19:54 +0530

    feat: implement progress component

---
+++ src/components/progress.tsx
@@ -0,0 +1,21 @@
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+import React from "react";
+
+type ProgressProps = {
+  progress: number;
+}
+
+export function Progress({ progress }: ProgressProps) {
+  return (
+    <ProgressPrimitive.Root
+      value={progress}
+      className="h-3 w-full overflow-hidden rounded-full bg-neutral-700"
+    >
+      <ProgressPrimitive.Indicator
+        style={{ width: `${progress}%` }}
+        className="h-full duration-300 ease-in-out dark:bg-white"
+      />
+    </ProgressPrimitive.Root>
+  );
+};
+
