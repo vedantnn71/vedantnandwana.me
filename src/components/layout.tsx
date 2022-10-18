4e92d3d5f9997b7ec3947df5a12ef30e34ce7b1a
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-18 18:40:34 +0530

    chore: use svg for background noise

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -16,6 +16,19 @@ export function Layout({ children }: LayoutProps) {
       </div>
 
       <div className="background-grid"></div>
+
+      <svg id="texture" className="w-screen h-screen fixed top-0 left-0 -z-50 opacity-20">
+        <filter id="noise">
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency=".8"
+            numOctaves="4"
+            stitchTiles="stitch"
+          ></feTurbulence>
+          <feColorMatrix type="saturate" values="0"></feColorMatrix>
+        </filter>
+        <rect width="100%" height="100%" filter="url(#noise)"></rect>
+      </svg>
     </>
   );
 };
