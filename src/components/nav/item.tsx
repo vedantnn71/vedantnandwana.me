2af89f20e042d803f78c09d4dd8aefd48dc3c2a0
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-19 14:44:15 +0530

    feat(nav): add hover animation on links

--- src/components/nav/item.tsx
+++ src/components/nav/item.tsx
@@ -17,7 +17,7 @@ export function NavItem({ href, children, className }: NavItemProps) {
     <Link href={href}>
       <a
         className={cx(
-          "px-3 text-base md:p-0 md:text-neutral-300 md:hover:text-white",
+          "px-3 text-base md:hover:text-white",
           isActive &&
             "rounded-xl bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 py-2 font-medium text-neutral-800 md:bg-none",
           className
