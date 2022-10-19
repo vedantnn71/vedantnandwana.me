b3b8daa4f98f723672674629939d38562b846d02
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-19 16:31:27 +0530

    perf: add next bundle analyzer

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    NEXT_PUBLIC_ENV: 'PRODUCTION',
+  },
 }
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+  
+module.exports = withBundleAnalyzer(nextConfig);
+
