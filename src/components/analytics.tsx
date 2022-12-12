2b98983bcf87b1e57aedfa148dd2d6f3a63a593b
Vedant Nandwana <70624701+vedantnn71@users.noreply.github.com>
2022-12-12 15:46:36 +0530

    fix(analytics): invalid script url

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -3,7 +3,7 @@ import Script from "next/script";
 export function Analytics() {
   return (
     <Script
-      src="https://spark-analytics.vercel.app/track.js"
+      src="https://usebud.vercel.app/track.js"
       data-id="clameuzd80000mb081x2ge2l9"
       strategy="afterInteractive"
     />
