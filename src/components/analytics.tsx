1f82d3a7d08c340e7d051ab206268e3afd0b197a
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-02-21 13:12:21 +0530

    fix(analytics): update url

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -3,8 +3,7 @@ import Script from "next/script";
 export function Analytics() {
   return (
     <Script
-      src="https://usebud.vercel.app/track.js"
-      data-id="clameuzd80000mb081x2ge2l9"
+      src="https://withbeacon.vercel.app/track.js"
       strategy="afterInteractive"
     />
   );
